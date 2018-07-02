2715193415d21c782923cac37abc173c0786567b
annajjj <ajonak@o2.pl>
2018-07-02 16:43:07 +0200

    ui start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
   alphabet = ["A", "B", "C", "D", "E", "F"];
@@ -155,4 +155,6 @@ export class Letter{
   addBit(bit: 0 |1){
     this.code = bit + this.code;
   }
+
+
 }
